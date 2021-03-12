448dc77d95ecbdb55fa5a637d7576cc4f1fd06d8
mpos2m <mpos2m@gmail.com>
2021-03-12 10:16:55 +0900

    DB연결 테스트

---
+++ src/test/java/erp/databases/JdbcConnTest.java
@@ -0,0 +1,19 @@
+package erp.databases;
+
+import java.sql.Connection;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class JdbcConnTest {
+
+	
+	@Test
+	public void testGetConnection() {
+		System.out.printf("%s()%n", "testGetConnection");
+		Connection con = JdbcConn.getConnection();
+		System.out.println("con : " + con);
+		Assert.assertNotNull(con);
+	}
+
+}
